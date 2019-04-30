3fff60a9de51f2de39784bd487f7276ced253d73
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-04-30 16:07:43 -0300

    test - implementa 5 novos testes para true

--- test/index.spec.js
+++ test/index.spec.js
@@ -9,5 +9,20 @@ describe('Validador de Cartão de Crédito', () => {
     it('Deve retornar false', () => {
         expect(cardValidator.cardValidator(6062822004739268)).to.equal("false")
     });
+    it('Deve retornar true', () => {
+        expect(cardValidator.cardValidator(4485071250980189)).to.equal("true")
+    });
+    it('Deve retornar true', () => {
+        expect(cardValidator.cardValidator(343532797404686)).to.equal("true")
+    });
+    it('Deve retornar true', () => {
+        expect(cardValidator.cardValidator(30201653051514)).to.equal("true")
+    });
+    it('Deve retornar true', () => {
+        expect(cardValidator.cardValidator(6011311458738973)).to.equal("true")
+    });
+    it('Deve retornar true', () => {
+        expect(cardValidator.cardValidator(3569676135946592)).to.equal("true")
+    });
 })
    
