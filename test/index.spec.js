1cd6c1aa5ad9126a725d9b1c8725c0fd77650522
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-05-07 11:55:27 -0300

    ajuste-function - ajuste retorno true false

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,31 +4,31 @@ const cardValidator = require("../lib/index.js")
 
 describe('Validador de Cartão de Crédito', () => {
     it('Deve retornar true', () => {
-        expect(cardValidator(5368371546136151)).to.equal("true")
+        expect(cardValidator(5368371546136151)).to.equal(true)
     });
     it('Deve retornar false', () => {
-        expect(cardValidator(6062822004739268)).to.equal("false")
+        expect(cardValidator(6062822004739268)).to.equal(false)
     });
     it('Deve retornar false', () => {
-        expect(cardValidator(6062822204795268)).to.equal("false")
+        expect(cardValidator(6062822204795268)).to.equal(false)
     });
     it('Deve retornar true', () => {
-        expect(cardValidator(4485071250980189)).to.equal("true")
+        expect(cardValidator(4485071250980189)).to.equal(true)
     });
     it('Deve retornar true', () => {
-        expect(cardValidator(343532797404686)).to.equal("true")
+        expect(cardValidator(343532797404686)).to.equal(true)
     });
     it('Deve retornar true', () => {
-        expect(cardValidator(30201653051514)).to.equal("true")
+        expect(cardValidator(30201653051514)).to.equal(true)
     });
     it('Deve retornar true', () => {
-        expect(cardValidator(6011311458738973)).to.equal("true")
+        expect(cardValidator(6011311458738973)).to.equal(true)
     });
     it('Deve retornar true', () => {
-        expect(cardValidator(3569676135946592)).to.equal("true")
+        expect(cardValidator(3569676135946592)).to.equal(true)
     });
-    it('Deve retornar true', () => {
-        expect(cardValidator(0000000000000000)).to.equal("false")
+    it('Deve retornar false', () => {
+        expect(cardValidator(0000000000000000)).to.equal(false)
     });
 })
    
